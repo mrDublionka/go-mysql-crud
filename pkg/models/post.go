ad4110e1b85b34589faf9d7404e84e148dfa7b14
Ion Cojocari <69515189+Cojocari-Ion@users.noreply.github.com>
2023-10-16 23:16:52 +0300

    first on goLand

--- pkg/models/post.go
+++ pkg/models/post.go
@@ -1,1 +1,17 @@
 package models
+
+import (
+	"blog-app/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+type Post struct {
+}
+
+var db *gorm.DB
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Post{})
+}
